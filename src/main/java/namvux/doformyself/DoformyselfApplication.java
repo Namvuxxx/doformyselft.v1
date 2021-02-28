b213db10db1e226996b56e33b91c7679f98080dc
namvu1 <namvs@ominext.com>
2021-02-28 15:15:34 +0700

    -1st

---
+++ src/main/java/namvux/doformyself/DoformyselfApplication.java
@@ -0,0 +1,13 @@
+package namvux.doformyself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoformyselfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoformyselfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/namvux/doformyself/DoformyselfApplicationTests.java
@@ -0,0 +1,13 @@
+package namvux.doformyself;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoformyselfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
